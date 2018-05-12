75514bdd626313ff6dfc334c41693dd9b7e60f56
hiren1995 <kadamhiren.sigma@gmail.com>
2018-05-12 16:56:57 +0530

    12/5/18 4:56pm

---
+++ Custom Files/ConstantValues.swift
@@ -0,0 +1,15 @@
+//
+//  ConstantValues.swift
+//  QuizApp
+//
+//  Created by Apple on 12/05/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+var userdefault = UserDefaults.standard
+
+
