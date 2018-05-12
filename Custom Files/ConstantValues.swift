3a979f7c1293a4015b003e190225381ff6d740da
hiren1995 <kadamhiren.sigma@gmail.com>
2018-05-12 18:42:59 +0530

    12/5/18 6:42pm

--- Custom Files/ConstantValues.swift
+++ Custom Files/ConstantValues.swift
@@ -12,4 +12,22 @@ import UIKit
 
 var userdefault = UserDefaults.standard
 
+var UserId = "UserId"
+var UserToken = "UserToken"
+var isLogin = "isLogin"
 
+var baseUrl = "https://bulale.in/quiz/index.php/api/"
+var signupAPI = "\(baseUrl)/user/user_register"
+var signinAPI = "\(baseUrl)/user/user_login"
+var verifyUserAPI = "\(baseUrl)/user/user_verify"
+var quizListAPI = "\(baseUrl)/user/quiz_list"
+var questionListAPI = "\(baseUrl)/user/quiz_question_list"
+var quizJoinAPI = "\(baseUrl)/user/quiz_join"
+var quizEndAPI = "\(baseUrl)/user/quiz_end"
+var leaderBoardListAPI = "\(baseUrl)/user/leader_board_list"
+var quizResultAPI = "\(baseUrl)/user/get_quiz_result"
+var leaderBoardQuizListAPI = "\(baseUrl)/user/leaderboard_quiz_list"
+var globalLeaderBoardAPI = "\(baseUrl)/user/global_quiz_result"
+var aboutUsDetailsAPI = "\(baseUrl)/user/about_us_details"
+var myQuizListAPI = "\(baseUrl)/user/my_quiz_list"
+var myQuizResultAPI = "\(baseUrl)/user/my_quiz_result"
--- QuizApp/SignUpViewController.swift
+++ QuizApp/SignUpViewController.swift
@@ -7,21 +7,55 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import MBProgressHUD
+import CropViewController
+import CoreLocation
 
-class SignUpViewController: UIViewController,UITextFieldDelegate {
+class SignUpViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,CropViewControllerDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet var imgProfilePic: UIImageView!
     @IBOutlet var txtFirstName: UITextField!
     @IBOutlet var txtLastName: UITextField!
     @IBOutlet var txtEmail: UITextField!
     @IBOutlet var txtContactNumber: UITextField!
     @IBOutlet var txtPassword: UITextField!
     
+    var imagePicker = UIImagePickerController()
+    
+    let locationManager = CLLocationManager()
+    
+    var latitude = CLLocationDegrees()
+    var longitude = CLLocationDegrees()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         addDoneButtonTextField()
         
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        latitude = locValue.latitude
+        longitude = locValue.longitude
     }
     
     @IBAction func btnNext(_ sender: UIButton) {
@@ -56,13 +90,92 @@ class SignUpViewController: UIViewController,UITextFieldDelegate {
             {
                 self.showAlert(title: "Alert", message: "Mobile Number must of 10 digits")
             }
+            else if(imgProfilePic.image == UIImage(named: "dummy"))
+            {
+                 self.showAlert(title: "Alert", message: "Please Select Profile Pic for your Account")
+            }
             else
             {
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let imgData = UIImageJPEGRepresentation(imgProfilePic.image!, 0.1)
                 
-                let verifyOTPViewController = storyboard.instantiateViewController(withIdentifier: "verifyOTPViewController") as! VerifyOTPViewController
+                let spinnerActivity = MBProgressHUD.showAdded(to: self.view, animated: true)
                 
-                self.present(verifyOTPViewController, animated: true, completion: nil)
+                let updateParameters:Parameters = ["user_name": txtFirstName.text! + txtLastName.text! , "user_contact_no" : txtContactNumber.text! , "user_email" : txtEmail.text! , "user_device_type" : 2 , "user_device_id" : "123", "user_device_token" : "abcd1234" , "user_lat" : latitude , "user_long" : longitude , "user_signin": 1 ,"user_password" : txtPassword.text!]
+                
+                
+                Alamofire.upload(multipartFormData: { (multipartFormData) in
+                    
+                    for (key, value) in updateParameters {
+                        multipartFormData.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+                    }
+                    
+                    if let data = imgData{
+                        
+                        multipartFormData.append(data, withName: "user_profile_photo", fileName: "image.jpg", mimeType: "image/jpg")
+                        
+                    }
+                    
+                },to: signupAPI, encodingCompletion: { (result) in
+                    
+                    switch result{
+                    case .success(let upload, _, _):
+                        upload.responseJSON { response in
+                            print("Succesfully uploaded")
+                            
+                            print(response.result.value)
+                            
+                            spinnerActivity.hide(animated: true)
+                            
+                            let tempDict = JSON(response.result.value)
+                            
+                            if(tempDict["status"].stringValue == "success" && tempDict["status_code"].intValue == 1)
+                            {
+                                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                                
+                                let verifyOTPViewController = storyboard.instantiateViewController(withIdentifier: "verifyOTPViewController") as! VerifyOTPViewController
+                                
+                                self.present(verifyOTPViewController, animated: true, completion: nil)
+                            }
+                            else if(tempDict["status"].stringValue == "failure" && tempDict["status_code"].intValue == 0)
+                            {
+                                
+                                let alert = UIAlertController(title: "User Already Registered", message: "Please Login as you are already registered from this mobile number", preferredStyle: .alert)
+                                
+                                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (alert) in
+                                    
+                                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                                    
+                                    let signInViewController = storyboard.instantiateViewController(withIdentifier: "signInViewController") as! SignInViewController
+                                    
+                                    self.present(signInViewController, animated: true, completion: nil)
+                                    
+                                }))
+                                
+                                self.present(alert, animated: true, completion: nil)
+                                
+                            }
+                            
+                            //let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                            
+                            //let verifyOTPViewController = storyboard.instantiateViewController(withIdentifier: "verifyOTPViewController") as! VerifyOTPViewController
+                            
+                            //self.present(verifyOTPViewController, animated: true, completion: nil)
+                            
+                        }
+                    case .failure(let error):
+                        print("Error in upload: \(error.localizedDescription)")
+                        spinnerActivity.hide(animated: true)
+                        self.showAlert(title: "Alert", message: "Error in Uploading")
+                        
+                    }
+                    
+                })
+                
+                //let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                
+                //let verifyOTPViewController = storyboard.instantiateViewController(withIdentifier: "verifyOTPViewController") as! VerifyOTPViewController
+                
+                //self.present(verifyOTPViewController, animated: true, completion: nil)
             }
         }
         
@@ -137,7 +250,91 @@ class SignUpViewController: UIViewController,UITextFieldDelegate {
         return false
         
     }
-
+    @IBAction func btnAddPic(_ sender: UIButton) {
+        
+        ChangeProfilePic()
+    }
+    
+    func ChangeProfilePic(){
+        
+        var alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        var cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.default)
+        {
+            UIAlertAction in
+            self.openCamera()
+        }
+        var gallaryAction = UIAlertAction(title: "Gallery", style: UIAlertActionStyle.default)
+        {
+            UIAlertAction in
+            self.openGallary()
+        }
+        var cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel)
+        {
+            UIAlertAction in
+        }
+        
+        imagePicker.delegate = self
+        alert.addAction(cameraAction)
+        alert.addAction(gallaryAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func openCamera()
+    {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alertWarning = UIAlertView(title:"Warning", message: "You don't have camera", delegate:nil, cancelButtonTitle:"OK", otherButtonTitles:"")
+            alertWarning.show()
+        }
+    }
+    func openGallary()
+    {
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            
+            //profileImg.image = image
+            
+            self.dismiss(animated: true, completion: nil)
+            let cropViewController = CropViewController(image: image)
+            cropViewController.delegate = self
+            present(cropViewController, animated: true, completion: nil)
+            
+            
+        } else{
+            print("Something went wrong")
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion:nil)
+    }
+    func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        
+        imgProfilePic.image = image
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
